50d85c522f3ab0ddc75e49e752a76d8ddfd5ce2b
Annie D <ading2@ualberta.ca>
2025-09-25 16:40:53 -0600

    Added colour

--- app/src/main/java/com/example/dangle/Shape.java
+++ app/src/main/java/com/example/dangle/Shape.java
@@ -3,6 +3,7 @@ package com.example.dangle;
 public abstract class Shape {
     private int x;
     private int y;
+    String color = "violet";
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
