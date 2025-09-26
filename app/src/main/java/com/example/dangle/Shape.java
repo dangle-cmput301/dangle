b066e11a360492b733cec8b84c8aff05a05fd9fe
Prem Elango <prem.el2005@gmail.com>
2025-09-26 10:35:46 -0600

    gitignore and colour

--- app/src/main/java/com/example/dangle/Shape.java
+++ app/src/main/java/com/example/dangle/Shape.java
@@ -4,7 +4,7 @@ public abstract class Shape {
     private int x;
     private int y;
 
-    private String color = "purple";
+    private String color = "blue";
 
     public Shape(int x, int y){
         this.x = x;
