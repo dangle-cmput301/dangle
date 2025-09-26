1e786af7c307f7307f64e93db33fbfc3be4c1449
CainanKG <122242192+CainanKG@users.noreply.github.com>
2025-09-25 19:00:30 -0600

    Revert "Cainan"

--- app/src/main/java/com/example/dangle/Shape.java
+++ app/src/main/java/com/example/dangle/Shape.java
@@ -3,8 +3,6 @@ package com.example.dangle;
 public abstract class Shape {
     private int x;
     private int y;
-
-    //private String color = "dark blue";
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
