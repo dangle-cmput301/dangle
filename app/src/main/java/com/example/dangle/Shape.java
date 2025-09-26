a2c398369a7aa7226325eb3c21addb9a28049b31
Prem Elango <prem.el2005@gmail.com>
2025-09-26 10:39:21 -0600

    black

--- app/src/main/java/com/example/dangle/Shape.java
+++ app/src/main/java/com/example/dangle/Shape.java
@@ -4,7 +4,7 @@ public abstract class Shape {
     private int x;
     private int y;
 
-    private String color = "blue";
+    private String color = "black";
 
     public Shape(int x, int y){
         this.x = x;
