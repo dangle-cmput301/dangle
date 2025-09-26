dda32192b16fda331261afd95f6ac6b57b0b538f
Annie D <ading2@ualberta.ca>
2025-09-25 21:20:17 -0600

    conflict

--- app/src/main/java/com/example/dangle/Shape.java
+++ app/src/main/java/com/example/dangle/Shape.java
@@ -3,7 +3,7 @@ package com.example.dangle;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "purple";
+    private String color = "violet";
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
