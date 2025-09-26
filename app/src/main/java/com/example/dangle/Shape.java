6f5ad2274ff7e3b794abf6669e34934fd40b71a4
Aditya S. <72671329+adityas0204@users.noreply.github.com>
2025-09-25 21:14:14 -0600

    Update Shape.java

--- app/src/main/java/com/example/dangle/Shape.java
+++ app/src/main/java/com/example/dangle/Shape.java
@@ -3,8 +3,8 @@ package com.example.dangle;
 public abstract class Shape {
     private int x;
     private int y;
-
-
+    private String color = "purple";
+    
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
